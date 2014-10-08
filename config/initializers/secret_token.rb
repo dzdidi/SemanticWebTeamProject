302287003e2993e9588d50fae84435aa46800a5e
dzdidi <deniszalessky@gmail.com>
2014-10-08 18:50:52 +0200

    Initial commit; Scaffold for book generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SemanticWebTeamProject::Application.config.secret_key_base = '1eea2c643a3d3fcba89b6fcde6a092fbb61d0247b309d22ac43c01ef0df6ece5877bd2d4e7e7b92a23f11a007f0c00f232b02d328aaf76424a2240628f03a3ad'
