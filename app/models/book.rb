25972eee6d44cd49d4f54f53ab23f779f4e2b4a7
MikeChang <mike781025@gmail.com>
2014-11-05 01:29:07 +0100

    clean the codes

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,5 @@
 require "net/http"
 class Book < ActiveRecord::Base
-<<<<<<< HEAD
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE #stupid trick
   app_id = "383644138456712"
   app_secret = "6098a751b6d9b829b245fef8ec1f3408"  
@@ -79,14 +78,22 @@ class Book < ActiveRecord::Base
 		#y = book.sale_info.each { |k, v| puts "Key: #{k}, Value: #{v}" }
 	end
 
-	def Book.testGoolge
+
+	def Book.searchGoolge(title)
 		#https://github.com/zeantsoi/GoogleBooks
-		books = GoogleBooks.search(9780345508553) 
+		
+		#from here
+		
+		books = GoogleBooks.search(title) 
 		book = books.first
 		data = book.sale_info
 		data["title"] = book.title
 		data["image_link"] = book.image_link
+		data["amazonurl"] = "http://www.amazon.com/s/ref=nb_sb_ss_c_0_4?url=search-alias%3Dstripbooks&field-keywords="+title.tr(" ","+");
 
+		
+		#finish
+		
 		return data
 		#y = data.each { |k, v| puts "Key: #{k}, Value: #{v}" }
 	end
